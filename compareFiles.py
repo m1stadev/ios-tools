3e57a46c1f7d99881b1ed5c756d4a9e72f97169d
marijuanARM <37033149+marijuanARM@users.noreply.github.com>
2020-11-25 13:28:52 -0600

    Add compareFiles.py script

---
+++ compareFiles.py
@@ -0,0 +1,53 @@
+#Copyright (c) 2020, @mcg29_
+#Code modified by marijuanARM
+
+#!/usr/bin/env python3
+
+import argparse
+import os
+import sys
+
+def main():
+	parser = argparse.ArgumentParser(description="compareFiles.py - A script to create a diff file between 2 extracted kernelcaches that img4lib can utilize.", usage="./compareFiles.py -i kcache.raw -o kcache.patched -d kc.bpatch")
+	parser.add_argument('-i', '--input', help='Input kernelcache', nargs=1)
+	parser.add_argument('-p', '--patched', help='Patched kernelcache', nargs=1)
+	parser.add_argument('-d', '--diff', help='Diff file to write to', nargs=1)
+	args = parser.parse_args()
+
+	if not args.input or not args.patched or not args.diff:
+		sys.exit(parser.print_help(sys.stderr))
+
+	if not os.path.isfile(args.input[0]):
+		sys.exit(f"[ERROR] Input kernel {args.input[0]} does not exist. Exiting...")
+
+	if not os.path.isfile(args.patched[0]):
+		sys.exit(f"[ERROR] Patched kernel {args.patched[0]} does not exist. Exiting...")
+
+	with open(args.patched[0], 'rb') as f:
+		patched = f.read()
+
+	with open(args.input[0], 'rb') as f:
+		original = f.read()
+
+	if len(patched) != len(original):
+		print("size does not match, can't compare files! exiting...")
+		sys.exit(f"[ERROR] Input kernel {args.input[0]} and patched kernel {args.patched[0]} are not the same size. Exiting...")
+
+	diff = []
+	for i in range(len(original)):
+		originalByte = original[i]
+		patchedByte = patched[i]
+
+		if originalByte != patchedByte:
+			diff.append([hex(i), hex(originalByte), hex(patchedByte)])
+
+	with open(args.diff[0], 'w+') as f:
+		f.write('#AMFI\n\n')
+		for x in diff:
+			data = f'{str(x[0])} {(str(x[1]))} {(str(x[2]))}'
+			f.write(f'{data}\n')
+
+	print(f'Patch file written to: {args.diff[0]}. Exiting...')
+
+if __name__ == "__main__":
+	main()
