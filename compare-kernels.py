6ef91d57946f04570f373d7ee1e87fcea736e8ae
marijuanARM <37033149+marijuanARM@users.noreply.github.com>
2021-01-22 11:55:37 -0600

    [compare-kernels.py] something something pep8 formatting

--- compare-kernels.py
+++ compare-kernels.py
@@ -1,52 +1,54 @@
 #!/usr/bin/env python3
 
-#Copyright (c) 2020, @mcg29_
-#Code modified by marijuanARM
+# Copyright (c) 2020, @mcg29_
+# Code modified by marijuanARM
 
 import argparse
 import os
 import sys
 
+
 def main():
-	parser = argparse.ArgumentParser(description="compare-kernels.py - A script to create a diff file between 2 raw kernelcaches that img4lib can utilize.")
-	parser.add_argument('-i', '--input', help='Input kernelcache', nargs=1)
-	parser.add_argument('-p', '--patched', help='Patched kernelcache', nargs=1)
-	parser.add_argument('-d', '--diff', help='Diff file to write to', nargs=1)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description="compare-kernels.py - Create an img4lib diff file between 2 raw kernelcaches.")
+    parser.add_argument('-i', '--input', help='Input kernelcache', nargs=1)
+    parser.add_argument('-p', '--patched', help='Patched kernelcache', nargs=1)
+    parser.add_argument('-d', '--diff', help='Diff file to write to', nargs=1)
+    args = parser.parse_args()
 
-	if not args.input or not args.patched or not args.diff:
-		sys.exit(parser.print_help(sys.stderr))
+    if not args.input or not args.patched or not args.diff:
+        sys.exit(parser.print_help(sys.stderr))
 
-	if not os.path.isfile(args.input[0]):
-		sys.exit(f"[ERROR] Input kernel {args.input[0]} does not exist. Exiting...")
+    if not os.path.isfile(args.input[0]):
+        sys.exit(f"[ERROR] Input kernel {args.input[0]} does not exist. Exiting...")
 
-	if not os.path.isfile(args.patched[0]):
-		sys.exit(f"[ERROR] Patched kernel {args.patched[0]} does not exist. Exiting...")
+    if not os.path.isfile(args.patched[0]):
+        sys.exit(f"[ERROR] Patched kernel {args.patched[0]} does not exist. Exiting...")
 
-	with open(args.patched[0], 'rb') as f:
-		patched = f.read()
+    with open(args.patched[0], 'rb') as f:
+        patched = f.read()
 
-	with open(args.input[0], 'rb') as f:
-		original = f.read()
+    with open(args.input[0], 'rb') as f:
+        original = f.read()
 
-	if len(patched) != len(original):
-		sys.exit(f"[ERROR] Input kernel {args.input[0]} and patched kernel {args.patched[0]} are not the same size. Exiting...")
+    if len(patched) != len(original):
+        sys.exit(f"[ERROR] Input kernel {args.input[0]} and patched kernel {args.patched[0]} are not the same size. Exiting...")
 
-	diff = []
-	for i in range(len(original)):
-		originalByte = original[i]
-		patchedByte = patched[i]
+    diff = []
+    for i in range(len(original)):
+        originalByte = original[i]
+        patchedByte = patched[i]
 
-		if originalByte != patchedByte:
-			diff.append([hex(i), hex(originalByte), hex(patchedByte)])
+        if originalByte != patchedByte:
+            diff.append([hex(i), hex(originalByte), hex(patchedByte)])
 
-	with open(args.diff[0], 'w+') as f:
-		f.write('#AMFI\n\n')
-		for x in diff:
-			data = f'{str(x[0])} {(str(x[1]))} {(str(x[2]))}'
-			f.write(f'{data}\n')
+    with open(args.diff[0], 'w+') as f:
+        f.write('#AMFI\n\n')
+        for x in diff:
+            data = f'{str(x[0])} {(str(x[1]))} {(str(x[2]))}'
+            f.write(f'{data}\n')
 
-	print(f'Diff file written to: {args.diff[0]}. Exiting...')
+    print(f'Diff file written to: {args.diff[0]}. Exiting...')
+
 
 if __name__ == "__main__":
-	main()
+    main()
